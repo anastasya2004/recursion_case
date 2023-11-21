9e10dd41c55c25ab5df7fdfbfbc306bb70a4614d
Alena <aaaaaalena00@mail.ru>
2023-11-21 19:02:06 +0700

    introductory part

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
+from turtle import *
 
+print("Привет! Какой фрактал вы хотите посмотреть? Выберите число")
+print("1 Рекурсивная функция")
+print("2 Построение двоичного дерева")
+print("3 Фрактал Ветка")
+print("4 Кривая Коха")
+print("5 Снежинка Коха")
+print("6 Кривая Минковского")
+print("7 Первая ледяная фигура")
+print("8 Вторая ледяная фигура")
+print("9 Кривая Леви")
+print("10 Фрактал Алёны")
+print("11 Фрактал Ангелины")
+print("12 Фрактал Анастасии")
+
+choice = input("\n")
+
+if choice == "1":
+    print("тут будет код")
+
+elif choice == "2":
+    print("тут будет код")
+
+elif choice == "3":
+    print("тут будет код")
+
+elif choice == "4":
+    print("тут будет код")
+
+elif choice == "5":
+    print("тут будет код")
+
+elif choice == "6":
+    print("тут будет код")
+
+elif choice == "7":
+    print("тут будет код")
+
+elif choice == "8":
+    print("тут будет код")
+
+elif choice == "9":
+    print("тут будет код")
+
+elif choice == "10":
+    print("тут будет код")
+
+elif choice == "11":
+    print("тут будет код")
+
+elif choice == "12":
+    print("тут будет код")
